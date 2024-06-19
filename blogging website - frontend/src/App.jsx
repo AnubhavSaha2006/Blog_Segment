f8d7df1b898e011d9737126e751a03c03623b3b6
Vedant Sinha <sinha.vedant25@gmail.com>
2024-06-20 02:34:03 +0530

    Initial commit

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,6 +1,34 @@
+import Navbar from "./components/navbar.component";
+import {Routes, Route} from "react-router-dom";
+import UserAuthForm from "./pages/userAuthForm.page";
+
+
+export const UserContext = createCoontext ({})
+
+
 const App = () => {
+
+    const [userAuth, setUserAuth] = useState();
+
+    useEffect (()=> {
+
+        let userInSession = lookInSession("user");
+        userInSession ? setUserAuth(SON.parse(userInSession)) : setUserauth ({ access_token : null} )
+    }, [])
+
+
     return (
-        <h1>MERN Blogging website by modern web</h1>
+
+
+        
+        <UserContext.Provider value={{UserAuth, setUserAuth }}>
+            <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path ="signin" element={<UserAuthForm type="sign-in" />} />
+                <Route path ="signup" element={<UserAuthForm type="sign-up" />} />
+                </Route>
+            </Routes>
+        </UserContext.Provider>
     )
 }
 
